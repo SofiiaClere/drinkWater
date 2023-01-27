aa8e42aa9447c97135be829628d6f514c1a56bf7
Sofiia Clere <105524628+SofiiaClere@users.noreply.github.com>
2023-01-27 17:20:00 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const smallCups = document.querySelectorAll('.cup-small');
-const liters = document.getElementById('.liters');
+const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained= document.getElementById('remained');
 
+updateBigCup();
+
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click',() => highLightCups(idx))
-})
+});
+
+function highLightCups(idx) {
+    if(smallCups[idx].classList.contains('full') && !smallCups [idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+};
 
 function highLightCups(idx) {
     smallCups.forEach((cup,idx2) => {
@@ -15,4 +23,29 @@ function highLightCups(idx) {
             cup.classList.remove('full')
         }
     })
-}
+
+    updateBigCup()
+};
+
+function updateBigCup () {
+    const fullCups = document.querySelectorAll('.cup-small.full').length 
+    const totalCups = smallCups.length
+
+    if(fullCups===0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    if (fullCups === totalCups) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible';
+        liters.innerText = `${2-(250 * fullCups / 1000)}`
+      }
+
+    }
